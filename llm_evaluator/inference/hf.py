9845368a05232e7da6196f14605651fdb5f69a08
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2025-07-03 23:15:34 +0800

    add hf inference

--- llm_evaluator/inference/hf.py
+++ llm_evaluator/inference/hf.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from accelerate import Accelerator
+from llm_evaluator.utils.type_utils import InferenceInput, InferenctOutput
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
@@ -24,4 +25,95 @@ class HuggingFaceInference(BaseInference):
         self.accelerator = Accelerator()
         self.model: PreTrainedModel = self.accelerator.prepare(model)
         self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.tokenizer.apply_chat_template()
+
+    def generate(self, inputs: list[InferenceInput]) -> list[InferenctOutput]:
+        tokenize_cfgs = self.inference_cfgs.get("tokenize_cfgs", {})
+        generate_cfgs = self.inference_cfgs.get("generate_cfgs", {})
+        messages = [input.conversation for input in inputs]
+        encoded_inputs = self.tokenizer.apply_chat_template(
+            conversation=messages,
+            add_generation_prompt=True,
+            return_tensors="pt",
+            return_dict=True,
+            padding=True,
+            **tokenize_cfgs,
+        )
+        outputs = self.model.generate(
+            input_ids=encoded_inputs["input_ids"].to(self.accelerator.device),
+            attention_mask=encoded_inputs["attention_mask"].to(self.accelerator.device),
+            num_return_sequences=1,
+            **generate_cfgs,
+        )
+        output_ids = [
+            output[encoded_inputs["input_ids"].shape[-1] :] for output in outputs
+        ]
+        responses = [
+            self.tokenizer.decode(output_id, skip_special_tokens=True)
+            for output_id in output_ids
+        ]
+        inference_outptus = [
+            InferenctOutput(
+                response=responses[idx],
+                input=inputs[idx].model_dump(),
+                engine="hf",
+                meta_data={
+                    "output_id": output_ids[idx].tolist(),
+                    "tokenize_cfgs": tokenize_cfgs,
+                    "generate_cfgs": generate_cfgs,
+                },
+            )
+            for idx in range(len(inputs))
+        ]
+        return inference_outptus
+
+
+def main() -> None:
+    import argparse
+
+    from ..utils.config import load_config, update_config_with_unparsed_args
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--config-file-path",
+        type=str,
+        required=True,
+        help="The path to the config file",
+    )
+    args, unparsed_args = parser.parse_known_args()
+
+    cfgs = load_config(args.config_file_path)
+    update_config_with_unparsed_args(unparsed_args=unparsed_args, cfgs=cfgs)
+
+    inference = HuggingFaceInference(
+        model_cfgs=cfgs["model_cfgs"], inference_cfgs=cfgs["inference_cfgs"]
+    )
+
+    inference_input = [
+        InferenceInput.from_prompts(
+            prompt="中国的首都是哪里？",
+            system_prompt="你是一个人工智能助手",
+        ),
+        InferenceInput.from_prompts(
+            prompt="Where is the capital of China?",
+            system_prompt="You are an AI assistant",
+        ),
+        InferenceInput.from_prompts(
+            prompt="中国有多少个省份？分别是哪些？",
+            system_prompt="你是一个人工智能助手",
+        ),
+        InferenceInput.from_prompts(
+            prompt="How many provinces are there in China? What are they?",
+            system_prompt="You are an AI assistant",
+        ),
+    ]
+
+    outputs = inference.generate(inference_input)
+
+    for inference_output in outputs:
+        print(inference_output.response)
+
+
+if __name__ == "__main__":
+    main()
